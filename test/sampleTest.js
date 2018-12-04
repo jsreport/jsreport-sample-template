4314a1d38c8f577ca70885704e16079c0ea331f3
BJR Matos <bjrmatos@gmail.com>
2018-12-03 20:13:01 -0500

    update samples to be grouped and show a hierarchy

--- test/sampleTest.js
+++ test/sampleTest.js
@@ -1,4 +1,5 @@
 const samples = require('../')
+const fsStore = require('jsreport-fs-store')
 const templates = require('jsreport-templates')
 const xlsx = require('jsreport-xlsx')
 const data = require('jsreport-data')
@@ -8,13 +9,16 @@ const handlebars = require('jsreport-handlebars')
 const chromePdf = require('jsreport-chrome-pdf')
 const pdfUtils = require('jsreport-pdf-utils')
 const jsreport = require('jsreport-core')
-const Promise = require('bluebird')
 
 describe('sample', function () {
   let reporter
 
   beforeEach(() => {
-    reporter = jsreport({ templatingEngines: { strategy: 'in-process' } })
+    reporter = jsreport({
+      store: { provider: 'fs' },
+      templatingEngines: { strategy: 'in-process' }
+    })
+
     reporter.use(templates())
     reporter.use(data())
     reporter.use(xlsx())
@@ -27,13 +31,23 @@ describe('sample', function () {
         args: ['--no-sandbox']
       }
     }))
+    reporter.use(fsStore())
     reporter.use(samples({createSamples: true}))
 
     return reporter.init()
   })
 
-  it('should be able to render all sample templates', () => {
+  afterEach(() => {
+    return reporter.close()
+  })
+
+  it('should be able to render all sample templates', async () => {
     const reports = ['Invoice', 'Orders', 'Population']
-    return Promise.all(reports.map((t) => reporter.render({ template: { name: t } })))
+
+    for (let n of reports) {
+      const folder = await reporter.folders.resolveFolderFromPath(`/${n}/main`)
+      const template = await reporter.documentStore.collection('templates').findOne({ name: 'main', folder: { shortid: folder.shortid } })
+      await reporter.render({ template: { shortid: template.shortid } })
+    }
   })
 })
